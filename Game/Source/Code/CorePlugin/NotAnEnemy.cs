770a2d221d5ee0c1e8b01ef6e41ef38f98fdb480
Adam <adam@adamslair.net>
2017-04-29 15:25:37 +0200

    Added NotAnEnemy

---
+++ Game/Source/Code/CorePlugin/NotAnEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Duality;
+using Duality.Components;
+using Duality.Editor;
+using Duality.Input;
+
+using Duality.Samples.Tilemaps.RpgLike;
+
+
+namespace Game
+{
+	[RequiredComponent(typeof(CharacterController))]
+	public class NotAnEnemy : Component, ICmpUpdatable
+	{
+		private Vector2 targetPos;
+
+		public Vector2 TargetPos
+		{
+			get { return this.targetPos; }
+			set { this.targetPos = value; }
+		}
+
+
+		void ICmpUpdatable.OnUpdate()
+		{
+			Transform transform = this.GameObj.Transform;
+			CharacterController character = this.GameObj.GetComponent<CharacterController>();
+
+			// Walk directly to the target
+			Vector2 diffToTarget = this.targetPos - transform.Pos.Xy;
+			Vector2 directionToTarget = diffToTarget.Normalized;
+			float targetDistance = diffToTarget.Length;
+
+			character.TargetMovement = directionToTarget;
+		}
+	}
+}
